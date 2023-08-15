51124bac41061c939f85b0dc6b9ba36f50f14f75
Mark Lang <marklang251@gmail.com>
2023-08-15 17:20:12 -0400

    added tests for tile flag and question toggles

--- test/test_tile.py
+++ test/test_tile.py
@@ -10,11 +10,42 @@ def tile():
     return tile
 
 @pytest.mark.tile
-def test_safe_tile(tile):
-    assert tile.coordinates == (3, 3)
+def test_tile_constructor(tile):
+    assert tile.get_coordinates() == (3, 3)
     assert tile.is_mine == False
     assert tile.num_adjacent_mines == None
     assert tile.is_flipped == False
     assert tile.is_flagged == False
     assert tile.is_question == False
+
+@pytest.mark.tile
+def test_simple_toggles(tile):
+    assert tile.is_flagged == False
+    assert tile.is_question == False
+    tile.toggle_flag()
+    assert tile.is_flagged == True
+    assert tile.is_question == False
+    tile.toggle_flag()
+    assert tile.is_flagged == False
+    assert tile.is_question == False
+    tile.toggle_question()
+    assert tile.is_flagged == False
+    assert tile.is_question == True
+    tile.toggle_question()
+    assert tile.is_flagged == False
+    assert tile.is_question == False
+
+@pytest.mark.tile
+def test_interacting_toggles(tile):
+    assert tile.is_flagged == False
+    assert tile.is_question == False
+    tile.toggle_flag()
+    tile.toggle_question()
+    assert tile.is_flagged == False
+    assert tile.is_question == True
+    tile.toggle_flag()
+    assert tile.is_flagged == True
+    assert tile.is_question == False
+
+
 
